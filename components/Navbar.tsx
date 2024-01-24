dd6a175106264b938427fbed44a8d4d5e655f81b
dcharles23 <144685783+dcharles23@users.noreply.github.com>
2024-01-24 15:32:09 +0000

    boarder removed from nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image"
+// import above gives faster load times
 import Link from "next/link"
 
 const navbar = () => {
   return (
     // used semantic html nav tag below for the navbar to improve SEO
-    <nav className="border-2 border-red-500 flexBetween max-container
+    <nav className="flexBetween max-container
      padding-container relative z-30 py-5">
       <Link href="/">
-        Home
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
     </nav>
   )
