629dd0e60e6369ccede3744bae0b37e5354daa3a
dcharles23 <144685783+dcharles23@users.noreply.github.com>
2024-01-24 17:08:44 +0000

    components set up done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 // This is my home page
 export default function Home() {
   return (
     <>
-    
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
     </>
   )
 }
