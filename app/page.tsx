6400767f6efbd9582c71747034fc84c7b4b75127
dcharles23 <144685783+dcharles23@users.noreply.github.com>
2024-01-24 17:05:51 +0000

    Hero, Camp, Features, GetApp & Guide Sections of Homepage added to components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 // This is my home page
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-500 text-2xl">
-      Hello World
-    </h1>
+    <>
+    
+    </>
   )
 }
  
